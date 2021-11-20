4c2cc5fe92450b582add32885929a8d04658b063
Tejinder Singh <dev.singhtejinder@gmail.com>
2021-11-20 10:36:41 -0500

    loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
       {
         test: /\.(js|jsx)$/,
